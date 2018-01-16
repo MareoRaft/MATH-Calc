b3868dcb724fdf4e3a09cd191419db79639d506e
Matthew Lancellotti <mareoraft@gmail.com>
2018-01-16 00:06:43 -0500

    put an error in a LaTeX file to see if the build fails.  Also made minor formatting/syntax change to build-pdfs.sh.

--- scripts/build-pdfs.sh
+++ scripts/build-pdfs.sh
@@ -3,15 +3,14 @@ for DIR in $(ls -d */ | grep ''); do
     echo "Moving to $DIR and compiling all .tex"
     cd "$DIR"
     for FI in $(ls | grep --regexp="\.tex$"); do
-	echo "$FI"
-	latexmk -pdf -interaction=nonstopmode -silent "$FI" > output.txt 2>&1
-	EXITCODE="$?"
-	if [ $EXITCODE > 0 ]
-	then
+        echo "$FI"
+        latexmk -pdf -interaction=nonstopmode -silent "$FI" > output.txt 2>&1
+        EXITCODE="$?"
+        if [ $EXITCODE > 0 ]; then
             cat output.txt
-	fi
-	echo "$EXITCODE" >> ../exit-codes.txt
-	echo "$FI $EXITCODE" >> ../exit-codes-with-filenames.txt
+        fi
+        echo "$EXITCODE" >> ../exit-codes.txt
+        echo "$FI $EXITCODE" >> ../exit-codes-with-filenames.txt
     done
     cd ..
     echo "Left $DIR"
